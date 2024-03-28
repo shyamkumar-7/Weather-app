a1cd71e9a4a00f5edf51b9def274a0315bace1b7
shyamkumar-7 <130834063+shyamkumar-7@users.noreply.github.com>
2024-03-28 15:35:33 +0530

    2nd changes

--- src/components/WeatherApp/Weatherapp.jsx
+++ src/components/WeatherApp/Weatherapp.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import './Weatherapp.css'
 import './Error.css'
 
-
-
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
 import cloud_icon from '../assets/cloud.png'
@@ -12,12 +10,6 @@ import rain_icon from '../assets/rain.png'
 import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
-
-// import clear01d from '../assets/01d.png'
-// import clear01n from '../assets/01n.png'
-// import fewclouds02n from '../assets/02n.png'
-// import fewclouds02d from '../assets/02d.png'
-
 
 import cry from '../assets/cry.png'
 
